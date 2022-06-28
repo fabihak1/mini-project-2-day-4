357cff9cc23f2a6a409a935943e764e6dce6fa78
Fabiha Khanam <Fabihakhanam70@gmail.com>
2022-06-28 11:37:15 -0400

    Initial Commit

---
+++ mini project 2 day 4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mini project 2 day 4
+//
+//  Created by Thania Khanam on 6/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
